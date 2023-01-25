2bd16bab665b98876acfff3f25d2bc50bdc19f3e
pradip baidya <pradipbaidya8888@gmail.com>
2023-01-25 22:31:54 +0530

    28 no question added

--- 28 Question.js
+++ 28 Question.js
@@ -19,12 +19,56 @@
 
 // (a)
 
-for (var i = 1; i <= 3; i++) {
-  // console.log("* ".repeat(i));
-}
+// for (var i = 1; i <= 3; i++) {
+//   // console.log("* ".repeat(i));
+// }
  //or
 
-function squareStar(){
+// function RectangleStar(){
+  
+  // for(let r=0; r<5; r++){ // Row
+  //   for(let j1=0; j1<r; j1++){
+  //     process.stdout.write("*")
+  //   }
+  //   console.log("*")
+  //  }
 
+
+//   for(let k=5; k>0; k--){ // Row
+//     for(let l=k; l>0; l--){
+//       process.stdout.write("*")
+//     }
+//     console.log("*")
+//   }
+// }
+// RectangleStar()
+
+
+// function squareStar(){
+//   for(i=0 ; i<=5; i++){
+//     for(let j=0; j<=5; j++){
+//       process.stdout.write("*")
+//     }
+//     console.log("*")
+//   }
+// }
+// squareStar()
+
+
+
+function pyramid(){
+  let n = 5
+ //Outer loop 
+  for(let i = 0; i<=n; i++){
+  // for spaceing   
+    for(let j = 0; j<=n-i; j++){
+      process.stdout.write(` `)
+    }
+//printing star
+    for(let k=0; k<2*i-1; k++){ 
+      process.stdout.write(`*`)
+    }
+    console.log()
+  }
 }
-squareStar()
+pyramid()
